1a42dea0649b06fe6ca3b3f27739bfd26f1a1cda
Devin <84913029+Dewiin@users.noreply.github.com>
2023-10-31 23:02:25 -0400

    card tweak

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main() {
     Card* point_card = new PointCard();
-    point_card->setInstruction('9');
+    point_card->setInstruction("9");
     
     point_card->Print();
 }

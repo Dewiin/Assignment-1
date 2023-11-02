c609692407fe024beca232b914839beac1fe0931
Devin <84913029+Dewiin@users.noreply.github.com>
2023-11-01 22:53:31 -0400

    changed ActionCard regex to only take one digit

--- ActionCard.cpp
+++ ActionCard.cpp
@@ -28,7 +28,7 @@ bool ActionCard::isPlayable() {
     }
     
     //Regex patterns
-    regex pattern(R"(^(DRAW|PLAY)\s(\d)\sCARD(S)?|REVERSE\sHAND|SWAP\sHAND\sWITH\sOPPONENT)");
+    regex pattern(R"((DRAW|PLAY)\s(\d)\sCARD(S)?|REVERSE\sHAND|SWAP\sHAND\sWITH\sOPPONENT)");
 
     return regex_match(getInstruction(), pattern);
 }
